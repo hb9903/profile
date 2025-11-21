98529ebe305d59002d75f93066fe9fb138ee2519
김혜빈 <bno23701@gmail.com>
2025-11-21 17:57:29 +0900

    진짜 끝

--- js/header.js
+++ js/header.js
@@ -18,4 +18,21 @@ document.addEventListener("DOMContentLoaded",()=>{
             li.classList.toggle("on")
         })
     });
+
+ // 마우스 휠을 올리거나 내릴 때 사용하는 소스코드
+    let lastScrollTop = 0
+    window.addEventListener("scroll",()=>{
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop // 브라우저의 호환성을 생각하여 스크롤바가 내려온 길이를 계산
+
+        if(scrollTop < lastScrollTop){
+            // 마우스 휠을 위로 굴렸을 때
+            hiddenLnb.classList.remove("on")
+        }else{
+            // 마우스 휠을 아래로 굴렸을 때
+           hiddenLnb.classList.remove("on")
+        }
+        lastScrollTop = scrollTop
+    })
+
+
 })
